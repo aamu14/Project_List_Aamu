af32bd4f3a78eb727346b02caae4488dcb782505
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2024-02-15 13:27:59 +0700

    Update Project_List.py

--- Project_List.py
+++ Project_List.py
@@ -28,13 +28,7 @@ url2 = "https://bit.ly/gbooks_analysis_st"
 st.header("""
 This is the project that I have already completed: \n       
 """) 
-st.subheader("""
-              1. R x Python Using Streamlit (In Indonesian)
-             """)
-st.markdown("""
-            This project was made to see how well Streamlit works when using 2 combined programming language, R and Python. You can see the result on this link: 
-            %s
-            """ % url1)
+
 st.subheader("""
              2. Google Books Analysis (In English)
              """)
@@ -42,3 +36,11 @@ st.markdown("""
             This project was created to demonstrate data cleaning, manipulation, and conduct a basic analysis through simple visualization. The primary objective is to emphasize the importance of having clean data and the necessity of manipulation to interpret and present the data effectively.  You can see the result on this link: 
             %s
             """ % url2)
+
+st.subheader("""
+              1. R x Python Using Streamlit (In Indonesian)
+             """)
+st.markdown("""
+            This project was made to see how well Streamlit works when using 2 combined programming language, R and Python. You can see the result on this link: 
+            %s
+            """ % url1)
