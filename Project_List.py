ebac0679a59ba64718c5bbe5d5c5dadbb202df2d
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2024-09-20 20:17:05 +0700

    Update Project_List.py

--- Project_List.py
+++ Project_List.py
@@ -23,9 +23,7 @@ st.markdown("""---""")
 
 url1 = "https://bit.ly/RxPy_Streamlit"
 url2 = "https://bit.ly/gbooks_analysis_st"
-url3 = "https://bit.ly/SalarySurveyHRDBCT3"
-url4 = "https://bit.ly/VisualizationEstimatedSalarySurvey"
-url5 = "https://bit.ly/3WGmBUv"
+url3 = "https://vizpowerbi1.streamlit.app/"
 
 st.markdown("""
 This is the project that I have already completed. I sorted it in reverse order to showcase the newest project at the beginning: \n       
@@ -36,36 +34,9 @@ st.subheader("""
              3. Visualization of Salary Survey HRDBCT 3.0 (In Indonesia)
              """)
 st.markdown("""
-This dashboard is inspired by the results of a salary survey, indicating that the data used is not real, but randomly generated. The reason for this is that the salary data available in the survey only includes the Q1, Median, and Q3 figures. I felt that this data would be insufficient for my practice, so I opted to use randomly generated salary data instead.
-
-The randomly generated data was created based on several considerations:
-a) Assuming that the generated data follows a normal distribution.
-b) Assuming that the sample median can be estimated as the same as the sample mean.
-c) Since the parameters require the number of data points, the mean, and the standard deviation (SD), the SD value was obtained using the formula SD = (Interquartile Range / 1.35).
-d) To prevent the salary values from being negative or too low, I decided to set a limit that the generated salary values should not be negative and should not deviate too far from the Q1 value.
-e) Finally, I used RStudio to help generate this data, with some assistance from ChatGPT when errors occurred.
-
-There is a new column replacing the Q1, Median, and Q3 values, called "estimated salary." Additionally, there is a new column to check whether the salary obtained is above or below the 2024 minimum wage (UMP) for each province.
-""")
-st.markdown("""
-Here is a sample image from my visualization results.
-            """)
-st.image("Visualization Sample.jpeg")
-
-st.markdown("""
-If you interested more, you can download the file that i provided below:
-            """)
-st.markdown("""Salary Survey HRDBACOT Document Result:         
+            This dashboard was created to show the visualization of the Salary Survey and how the dashboard interacted. You can see the result on this link: 
             %s
             """ % url3)
-
-st.markdown("""Visualization of Estimated Salary Survey with Power BI:
-            %s
-            """ % url4)
-
-st.markdown("""Cleaned Dataset of Estimated Salary Survey:        
-            %s
-            """ % url5)
 #------------------------------------------------------------------------------
 st.subheader("""
              2. Google Books Analysis (In English)
